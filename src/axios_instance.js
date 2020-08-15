517592da3f1a787a04edb3d23bf648ae32d4a2c9
Nurbek <nuronbeck@gmail.com>
2020-08-16 03:24:20 +0500

    production baseUrl

--- src/axios_instance.js
+++ src/axios_instance.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 const instance = axios.create({
-    baseURL: 'http://localhost:3000'
+    baseURL: 'https://contacts-demo-vue.herokuapp.com/' //'http://localhost:3000'
 })
 export default instance
