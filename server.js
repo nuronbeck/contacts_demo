c974ddbbc90158c741410a60c175598918762190
Nurbek <nuronbeck@gmail.com>
2020-08-16 03:56:12 +0500

    fix server

--- server.js
+++ server.js
@@ -8,12 +8,12 @@ const app = express()
 //here we are configuring dist to serve app files
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
+app.use('/api', jsonServer.router('db.json'))
+
 // this * route is to serve project on different page routes except root `/`
 app.get(/.*/, function (req, res) {
 	res.sendFile(path.join(__dirname, '/dist/index.html'))
 })
-
-app.use('/api', jsonServer.router('db.json'));
 
 const port = process.env.PORT || 8080
 app.listen(port)
